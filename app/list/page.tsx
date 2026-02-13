a82d7b9e2bed8b574ce815ea3cdb290def74f117
Skhaton23 <skhaton237@gmail.com>
2026-02-13 14:31:56 -0500

    Updated page.tsx

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -3,13 +3,13 @@
 import { useEffect, useState } from 'react'
 import { supabase } from '@/lib/supabaseClient'
 
-export default function ListPage() {
+export default funtion ListPage () {
   const [items, setItems] = useState<any[]>([])
 
-   useEffect(() => {
-    fetchData()
+    useEffect(() => {
+        fetchData()
     }, [])
-
+     
     async function fetchData() {
         const { data, error } = await supabase
             .from('posts')
@@ -22,20 +22,18 @@ export default function ListPage() {
         setItems(data || [])
     }
 
-
     return (
         <div style={{ padding: '2rem' }}>
-            <h1>Supabase Data</h1>
-
+            <h1>Spabase Data</h1>
             {items.length === 0 && <p>No data found.</p>}
 
             <ul>
                 {items.map((item) => (
                     <li key={item.id}>
-                        <pre>{JSON.stringify(item, null, 2)}</pre>
+                        <prev>{JSON.stringify(items, null, 2)}</prev>
                     </li>
                 ))}
             </ul>
         </div>
     )
-}  
+}
