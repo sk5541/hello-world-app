281c1d0006a18f12f92089961e3924ea98555488
Skhaton23 <skhaton237@gmail.com>
2026-02-14 22:50:45 -0500

    Updated Assignment 4

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -3,12 +3,19 @@
 import { useEffect, useState } from 'react'
 import { supabase } from '@/lib/supabaseClient'
 
-export default funtion ListPage () {
+export default function ListPage() {
   const [items, setItems] = useState<any[]>([])
+  const [user, setUser] = useState<any>(null)
 
     useEffect(() => {
+        getUser()
         fetchData()
     }, [])
+
+    async function getUser() {
+        const { data } = await supabase.auth.getUser()
+        setUser(data.user)
+    }
      
     async function fetchData() {
         const { data, error } = await supabase
@@ -22,15 +29,47 @@ export default funtion ListPage () {
         setItems(data || [])
     }
 
+    async function handleVote(postId: number, voteValue: number) {
+        if(!user){
+            alert('You must be logged in to vote')
+            return
+        }
+
+        const { error } = await supabase
+            .from('caption_votes')
+            .insert([
+                {
+                    caption_id: postId,
+                    user_id: user.id,
+                    vote: voteValue,
+                }
+            ])
+        if (error) {
+            console.error('Vote error:', error.message)
+            alert('Error submitting vote')
+        } else {
+            alert('Vote submitted!')
+        }
+    }
+
     return (
         <div style={{ padding: '2rem' }}>
-            <h1>Spabase Data</h1>
+            <h1>Supabase Data</h1>
+            {!user && <p>Login to rate captions</p>}
             {items.length === 0 && <p>No data found.</p>}
 
             <ul>
                 {items.map((item) => (
-                    <li key={item.id}>
-                        <prev>{JSON.stringify(items, null, 2)}</prev>
+                    <li key={item.id} style={{ marginBottom: '1.5rem' }}>
+                        <pre>{JSON.stringify(item, null, 2)}</pre>
+
+                        <button onClick={() => handleVote(item.id, 1)}>
+                            👍 Like
+                        </button>
+
+                        <button onClick={() => handleVote(item.id, -1)}>
+                            👎 Dislike
+                        </button>
                     </li>
                 ))}
             </ul>
